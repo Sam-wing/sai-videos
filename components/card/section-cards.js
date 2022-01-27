9ca137ec4bf083dc919de809cd20736fc7f467cd
saiwang <saiwang1998@gmail.com>
2022-01-27 20:45:46 +0800

    fix

--- components/card/section-cards.js
+++ components/card/section-cards.js
@@ -8,7 +8,7 @@ const SectionCards = (props) => {
       <h2 className={styles.title}>{title}</h2>
       <div className={styles.cardWrapper}>
           {videos.map((video, idx) => {
-            return <Card id={idx} imgUrl={video.imgUrl} size={size} />
+            return <Card key={idx} id={idx} imgUrl={video.imgUrl} size={size} />
           })}
       </div>
     </section>
