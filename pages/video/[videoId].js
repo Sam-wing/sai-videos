c4b95875be5dc23ce9ba9f91b2bd5df62d0c929e
saiwang <saiwang1998@gmail.com>
2022-02-01 21:03:41 +0800

    add react modal

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -34,7 +34,7 @@ const Video = () => {
           width="100%"
           height="360"
           src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
-          frameborder="0"
+          frameBorder="0"
         ></iframe>
         <div className={styles.modalBody}>
             <div className={styles.modalBodyContent}>
